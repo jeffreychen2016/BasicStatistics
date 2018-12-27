72791a14ed4fdec6af40490b78b7fa439c0d7f1a
Jeffrey Chen <jeffreychen2016@gmail.com>
2018-12-26 18:51:44 -0600

    Add project files.

---
+++ BasicStatistics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BasicStatistics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
